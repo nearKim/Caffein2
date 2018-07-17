14a0c1c9ca70d80a03ab8e5dcb3147e1da7bc1a8
nearKim <garfield@snu.ac.kr>
2018-07-17 18:20:43 +0900

    Fix User password not hashed issue. Add Custom User Admin. Add Custom UserCreation/Change Form.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,43 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import ugettext_lazy as _
+
+from accounts.forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import (
-    User,
     ActiveUser
 )
 
-admin.site.register(User)
+User = get_user_model()
+
+
+class UserAdmin(BaseUserAdmin):
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+
+    list_display = ('email', 'name', 'phone', 'student_no', 'college',
+                    'department', 'category')
+    list_filter = ('college', 'department', 'category')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('개인정보'), {'fields': ('name', 'phone', 'profile_pic')}),
+        (_('학적정보'), {'fields': ('student_no', 'college', 'department', 'category')}),
+        (_('권한'), {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        (_('중요 날짜'), {'fields': ('join_year', 'join_semester', 'last_login')})
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password')}),
+        (_('개인정보'), {'fields': ('name', 'phone', 'profile_pic')}),
+        (_('학적정보'), {'fields': ('student_no', 'college', 'department', 'category')}),
+        (_('권한'), {'fields': ('is_active', 'is_staff')}),
+        (_('중요 날짜'), {'fields': ('join_year', 'join_semester')})
+    )
+    search_fields = ('email',)
+    ordering = ('date_joined',)
+
+
+# Now register the new UserAdmin...
+admin.site.register(User, UserAdmin)
 admin.site.register(ActiveUser)
--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,57 @@
 from django import forms
-from .models import User
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import (
+    UserCreationForm,
+    UserChangeForm,
+    ReadOnlyPasswordHashField)
+from django.utils.translation import ugettext_lazy as _
 
 
-class AuthenticationForm(forms.ModelForm):
+class CustomUserCreationForm(UserCreationForm):
+    error_messages = {
+        'password_mismatch': _('비밀번호가 일치하지 않습니다.')
+    }
+    password1 = forms.CharField(
+        label=_("비밀번호"),
+        widget=forms.PasswordInput,
+    )
+    password2 = forms.CharField(
+        label=_("비밀번호 확인"),
+        widget=forms.PasswordInput,
+        help_text=_("확인을 위해 비밀번호를 다시한번 입력해주세요."),
+    )
+
     class Meta:
-        model = User
-        fields = ['email', 'password']
+        model = get_user_model()
+        fields = ['rule_confirm', 'email', 'password1', 'password2', 'name', 'phone', 'student_no', 'college',
+                  'department', 'category', 'profile_pic', 'join_year', 'join_semester']
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError(_("Passwords don't match"))
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class CustomUserChangeForm(UserChangeForm):
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = get_user_model()
+        fields = ['rule_confirm', 'email', 'password', 'name', 'phone', 'student_no', 'college', 'department',
+                  'category', 'profile_pic', 'join_year', 'join_semester']
+
+    def clean_password(self):
+        # Regardless of what the user provides, return the initial value.
+        # This is done here, rather than on the field, because the
+        # field does not have access to the initial value
+        return self.initial["password"]
