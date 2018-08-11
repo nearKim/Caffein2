0b05281589a229046010e0f9504a839005ea9e7f
nearKim <garfield@snu.ac.kr>
2018-08-11 18:51:50 +0900

    Fix model field references. CharField and TextFields must not have null=True option. https://wayhome25.github.io/django/2017/09/23/django-blank-null/

--- cafe/models.py
+++ cafe/models.py
@@ -38,26 +38,26 @@ class Cafe(TimeStampedMixin):
         ('LAST', '마지막')
     )
 
-    name = models.CharField(_('카페이름'), max_length=50, unique=True)
-    address = models.CharField(_('주소'), max_length=100)
-    description = models.TextField(_('설명'), null=True, blank=True)
-    phone = models.CharField(_('전화번호'), max_length=14, null=True, blank=True)
-    machine = models.CharField(_('에스프레소 머신'), max_length=100, null=True, blank=True)
-    grinder = models.CharField(_('그라인더'), max_length=100, null=True, blank=True)
-    price = models.CharField(_('가격대'), max_length=15, choices=PRICE_CATEGORY, null=True, blank=True)
+    name = models.CharField(_('카페이름'), max_length=50)
+    address = models.CharField(_('주소'), max_length=200)
+    description = models.TextField(_('설명'), blank=True)
+    phone = models.CharField(_('전화번호'), max_length=14, blank=True)
+    machine = models.CharField(_('에스프레소 머신'), max_length=100, blank=True)
+    grinder = models.CharField(_('그라인더'), max_length=100, blank=True)
+    price = models.CharField(_('가격대'), max_length=15, choices=PRICE_CATEGORY, blank=True)
 
     from_time = models.TimeField(_('개점시간'), null=True, blank=True)
     to_time = models.TimeField(_('폐점시간'), null=True, blank=True)
 
-    closed_day = models.CharField(_('휴무일'), choices=DAY_CATEGORY, max_length=3, null=True, blank=True)
-    closed_frq = models.CharField(_('휴무 빈도'), choices=FRQ_CATEGORY, max_length=6, null=True, blank=True)
+    closed_day = models.CharField(_('휴무일'), choices=DAY_CATEGORY, max_length=3,  blank=True)
+    closed_frq = models.CharField(_('휴무 빈도'), choices=FRQ_CATEGORY, max_length=6, blank=True)
     closed_holiday = models.BooleanField(_('공휴일 휴무 여부'), default=False)
     uploader = models.ForeignKey(User, on_delete=models.CASCADE, related_name='uploader')
     image = models.ImageField(_('이미지'), upload_to=get_cafe_photo_path, blank=True)
 
     # naver api
     link = models.CharField(_('홈페이지'), validators=[URLValidator()], blank=True, max_length=260)
-    road_address = models.CharField(_('도로명주소'), max_length=100, null=True, blank=True)
+    road_address = models.CharField(_('도로명주소'), max_length=100, blank=True)
     mapx = models.IntegerField(_('x좌표'), null=True, blank=True)
     mapy = models.IntegerField(_('y좌표'), null=True, blank=True)
 
