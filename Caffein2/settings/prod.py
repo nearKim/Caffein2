1f862a1a7886501b27ecc57ad983de66bc56f72e
nearKim <garfield@snu.ac.kr>
2018-09-07 12:55:21 +0900

    Add fixed migration. Fix Email setting

--- Caffein2/settings/prod.py
+++ Caffein2/settings/prod.py
@@ -10,7 +10,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST_USER = 'snucoffee@gmail.com'
-EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+if DEBUG:
+    EMAIL_HOST_PASSWORD = get_secret('GMAIL_PASS')
+else:
+    EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
