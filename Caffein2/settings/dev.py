f698308c0666d80644bb7db1a38c4ebf4f08f913
nearKim <garfield@snu.ac.kr>
2018-09-11 08:48:10 +0900

    Truncate facebook secrets

--- Caffein2/settings/dev.py
+++ Caffein2/settings/dev.py
@@ -3,12 +3,12 @@ import facebook
 
 # Facebook
 # snucoffee계정
-APP_ID = 1960145344283234
-APP_SECRET = '377f41d4e8a22eab6d874a1a4ddcc32e'
+APP_ID = get_secret('FACEBOOK_APP_ID')
+APP_SECRET = get_secret('FACEBOOK_APP_SECRET')
 # FIXME: 이 그룹은 현재 snucoffee계정이 가입한 django_test 그룹이다.
 FACEBOOK_GROUP_ID = '542458402875116'  # django_test
 # FIXME: 이 토큰은 2개월후 만료된다.
-FACEBOOK_TOKEN = 'EAAb2veb0QmIBALZBusSFIlhPBSnDIPWVDLrzciiIIntKEsZCHSZAWNMZAUAeHtZCatzl1FUDhmPupSSZBMF4pNWUnFH5TE98EPGhX9qoUAUV3wGLNkpvZC4WIlI0fieVRWYEaZA1eYLy97qytfqnCZCJt1SDqdZCsfYZB4ZD'
+FACEBOOK_TOKEN = get_secret('FACEBOOK_APP_TOKEN_60')
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
