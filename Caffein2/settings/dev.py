ec00ca9f89397e8579de41ab85bc08e48ad93ab5
kwangdeokpark <pkd0330@snu.ac.kr>
2018-09-21 16:48:20 +0900

    fix surveys typo

--- Caffein2/settings/dev.py
+++ Caffein2/settings/dev.py
@@ -1,5 +1,14 @@
 from .base import *
+import facebook
 
+# Facebook
+# snucoffee계정
+APP_ID = get_secret('FACEBOOK_APP_ID')
+APP_SECRET = get_secret('FACEBOOK_APP_SECRET')
+# FIXME: 이 그룹은 현재 snucoffee계정이 가입한 django_test 그룹이다.
+FACEBOOK_GROUP_ID = '542458402875116'  # django_test
+# FIXME: 이 토큰은 2개월후 만료된다.
+FACEBOOK_TOKEN = get_secret('FACEBOOK_APP_TOKEN_60')
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
