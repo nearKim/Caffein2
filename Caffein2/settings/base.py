4ff3af72e5105f5c3a6cd4445cda6fd5e76c1d14
nearKim <garfield@snu.ac.kr>
2018-09-21 12:58:38 +0900

    Fix navermap now showing issue in coffee education detail view

--- Caffein2/settings/base.py
+++ Caffein2/settings/base.py
@@ -140,10 +140,9 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 # Login settings
-LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 
 # Static files (CSS, JavaScript, Images)
