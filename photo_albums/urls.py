7be5dfefc1feab14490dccd7e89a68365c2993ad
nearKim <garfield@snu.ac.kr>
2018-10-02 11:05:37 +0900

    Refactor initialize

--- photo_albums/urls.py
+++ photo_albums/urls.py
@@ -4,12 +4,13 @@ from photo_albums.views import *
 app_name = 'photo_albums'
 
 urlpatterns = [
-    path('', AlbumListView.as_view(), name='album_list'),
-    path('album/<int:pk>', AlbumDetailView.as_view(), name='album_detail'),
-    path('album_create', AlbumCreateView.as_view(), name='album_create'),
-    path('album_edit/<int:album_id>', AlbumEditView.as_view(), name='album_edit'),
+    path('', AlbumListView.as_view(), name='album-list'),
+    path('album/<int:pk>', AlbumDetailView.as_view(), name='album-detail'),
+    path('create/', AlbumCreateView.as_view(), name='album-create'),
+    path('edit/<int:album_id>', AlbumEditView.as_view(), name='album-edit'),
+    path('delete-photo/<int:pk>', delete_photo, name='delete-photo'),
+
     path('clear/<int:pk>', clear_database, name='clear_database'),
-    path('delete-photo/<int:pk>', delete_photo, name='delete_photo'),
     path('delete-photo-to-detail/<int:pk>', delete_photo_to_album_detail, name='delete_photo_to_detail'),
     path('photo/<int:pk>', PhotoDetailView.as_view(), name='photo_detail'),
 ]
