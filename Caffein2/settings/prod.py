8fd140e5d98a3cb950c9fb69410fef10345dde47
nearKim <garfield@snu.ac.kr>
2018-10-17 21:19:01 +0900

    awskey changed

--- Caffein2/settings/prod.py
+++ Caffein2/settings/prod.py
@@ -24,7 +24,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'Caffein2', 'static'),
 ]
 
-AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = 'caffein-alpha-assets'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
