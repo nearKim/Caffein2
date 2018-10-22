be83bae8990068a600ee1cd013c9ad2f23ead311
nearKim <garfield@snu.ac.kr>
2018-10-22 21:14:36 +0900

    Clean migrations and seeds

--- seeddb.sh
+++ seeddb.sh
@@ -2,16 +2,16 @@
 echo 'Seed initial DB with user, activeuser, os data...'
 
 echo 'Seeding user...'
-python manage.py loaddata accounts.user.json &&
+python manage.py loaddata ./seeds/accounts.user.json &&
 echo 'Seeding activeuser...'
-python manage.py loaddata accounts.activeuser.json &&
+python manage.py loaddata ./seeds/accounts.activeuser.json &&
 echo 'Seeding partners...'
-python manage.py loaddata partners.json &&
+python manage.py loaddata ./seeds/partners.json &&
 echo 'Seeding core...'
-python manage.py loaddata core.json &&
+python manage.py loaddata ./seeds/core.json &&
 echo 'Seeding sites...'
-python manage.py loaddata sites.json &&
+python manage.py loaddata ./seeds/sites.json &&
 echo 'Seeding surveys...'
-python manage.py loaddata surveys.json
+python manage.py loaddata ./seeds/surveys.json
 
 echo 'Done seeding.'
