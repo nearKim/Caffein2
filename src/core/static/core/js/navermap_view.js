d30ef4dc2803e4524c036c40c344213f0f74563a
nearKim <garfield@snu.ac.kr>
2018-11-13 21:35:21 +0900

    Delete infowindow from detailview navermap

--- src/core/static/core/js/navermap_view.js
+++ src/core/static/core/js/navermap_view.js
@@ -1,6 +1,6 @@
 /*
 OfficialMeeting, CoffeeEducation DetailView에서 사용할 네이버지도 자바스크립트.
-각 템플릿에서 장소의 mapx, mapy, 장소명을를 각각 x,y,location 변수로 받아야 한다.
+각 템플릿에서 장소의 mapx, mapy, 장소명을를 각각 x,y 변수로 받아야 한다.
  */
 
 let position = new naver.maps.Point(x, y)
@@ -18,16 +18,4 @@ marker = new naver.maps.Marker({
     map: map,
     position: position,
     zIndex: 100,
-})
-infoWindow = new naver.maps.InfoWindow({
-    content: location_title,
-    backgroundColor: "#eee",
-    anchorSize: new naver.maps.Size(30, 30),
-    anchorSkew: true,
-    anchorColor: "#eee",
-
-    pixelOffset: new naver.maps.Point(20, -20)
-})
-naver.maps.Event.addListener(marker, 'click', () => {
-    infoWindow.getMap() ? infoWindow.close() : infoWindow.open(map, marker)
 })
