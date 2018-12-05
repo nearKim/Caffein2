3f1426d71376526ca2c03a4ee55f2a4c841b0f7c
nearKim <garfield@snu.ac.kr>
2018-12-05 18:19:28 +0900

    Add gitignore and migrations

--- src/cafes/migrations/0001_initial.py
+++ src/cafes/migrations/0001_initial.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Cafe',
+            name='CafeList',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('created', models.DateTimeField(auto_now_add=True)),
